aee3340fe1999c91b257c99bf76cf775887740ae
Gurucharan94 <guru11294@gmail.com>
2019-10-08 10:52:55 +0530

    Code of Conduct

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,7 @@ $(function() {
     
     //===== Prealoder
     
-    $(window).on('load', function(event) {
-        $('.preloader').delay(500).fadeOut(500);
-    });
+   
     
     
     //===== Sticky
