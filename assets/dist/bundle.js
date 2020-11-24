c1f6f5ca51c01148b2f174360fac9051065c2428
Anbu Mani <anbumani91@hotmail.com>
2020-11-24 08:01:42 +0530

    Added Live Button

--- assets/dist/bundle.js
+++ assets/dist/bundle.js
@@ -159,6 +159,17 @@ $(function () {
 
     });
 
+    $(document).ready(function () {
+        //$("#delCookie").click(function(){
+        //    del_cookie("cookie");   
+        //}); 
+        $("#exampleModalCenter").modal("show");
+        var expire = new Date();
+        expire = new Date(expire.getTime() + 7776000000);
+        document.cookie = "cookie=here; expires=" + expire;
+
+    });
+
     $(window).on('scroll', function (event) {
 
         if ($('#navbarlogo').hasClass('navbar-area navbar-two sticky')) {
@@ -180,6 +191,24 @@ $(function () {
         }
     });
 
+    function getCookie(c_name) {
+        var c_value = document.cookie;
+        var c_start = c_value.indexOf(" " + c_name + "=");
+        if (c_start == -1) {
+            c_start = c_value.indexOf(c_name + "=");
+        }
+        if (c_start == -1) {
+            c_value = null;
+        } else {
+            c_start = c_value.indexOf("=", c_start) + 1;
+            var c_end = c_value.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = c_value.length;
+            }
+            c_value = unescape(c_value.substring(c_start, c_end));
+        }
+        return c_value;
+    }
     //===== Section Menu Active
 
     var scrollLink = $('.page-scroll');
